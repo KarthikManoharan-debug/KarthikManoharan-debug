90136335165e19b5f8dc1a272e32ddf80ed2c219
karthik Manoharan <karthik.manoharan@goodworklabs.com>
2022-02-27 16:57:15 +0530

    Updated

--- src/main/java/BrowserOptions.java
+++ src/main/java/BrowserOptions.java
@@ -1,11 +1,18 @@
 import org.openqa.selenium.chrome.ChromeOptions;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class BrowserOptions {
 
     public static ChromeOptions setChromeOptions() {
         ChromeOptions options = new ChromeOptions();
         options.setExperimentalOption("userAutomationExtension",false);
         options.addArguments("--start-maximized");
+        options.addArguments("--disable-web-security");
+        options.addArguments("--no-proxy-server");
+        Map<String,Object> prefs= new HashMap<String,Object>();
+
         return options;
 
     }
