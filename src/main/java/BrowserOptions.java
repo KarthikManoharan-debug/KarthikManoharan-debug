5dc58a6e964e77a6377d2db5cc3a6a93b124d6a3
karthik Manoharan <karthik.manoharan@goodworklabs.com>
2022-02-27 13:24:12 +0530

    Updated

--- src/main/java/BrowserOptions.java
+++ src/main/java/BrowserOptions.java
@@ -1,2 +1,12 @@
+import org.openqa.selenium.chrome.ChromeOptions;
+
 public class BrowserOptions {
+
+    public static ChromeOptions setChromeOptions() {
+        ChromeOptions options = new ChromeOptions();
+        options.setExperimentalOption("userAutomationExtension",false);
+        options.addArguments("--start-maximized");
+        return options;
+
+    }
 }
